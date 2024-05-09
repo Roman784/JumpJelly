72f02bc6c68aa3a33d90a49300e64d5617348dd8
Roman784 <rmironenko50@gmail.com>
2024-05-09 17:16:42 +0300

    Player double jump

--- Assets/_Project/Develop/Player/PlayerJumping.cs
+++ Assets/_Project/Develop/Player/PlayerJumping.cs
@@ -5,6 +5,9 @@ public class PlayerJumping : MonoBehaviour
 {
     [SerializeField] private float _force;
 
+    [SerializeField] private int _maxJumpCount;
+    private int _jumpCounter;
+
     [Space]
 
     [SerializeField] private PlayerMovement _movement;
@@ -13,14 +16,28 @@ public class PlayerJumping : MonoBehaviour
 
     private Rigidbody2D _rigidbody;
 
+    private void OnEnable()
+    {
+        _collisionHandler.OnWallTouched += OnWallTouched;
+        _collisionHandler.OnGroundTouched += OnGroundTouched;
+    }
+
+    private void OnDisable()
+    {
+        _collisionHandler.OnWallTouched -= OnWallTouched;
+        _collisionHandler.OnGroundTouched -= OnGroundTouched;
+    }
+
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+
+        RestoreJumpCount();
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0) || Input.GetKey(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
         }
@@ -28,7 +45,7 @@ public class PlayerJumping : MonoBehaviour
 
     private void Jump()
     {
-        if (_collisionHandler.IsInAir) return;
+        if (_collisionHandler.IsInAir && _jumpCounter <= 0) return;
 
         if (_collisionHandler.IsWallTouch)
         {
@@ -38,5 +55,22 @@ public class PlayerJumping : MonoBehaviour
 
         _rigidbody.velocity = Vector2.zero;
         _rigidbody.AddForce(transform.up * _force, ForceMode2D.Impulse);
+
+        _jumpCounter -= 1;
+    }
+
+    private void RestoreJumpCount()
+    {
+        _jumpCounter = _maxJumpCount;
+    }
+
+    private void OnWallTouched(RaycastHit2D hit)
+    {
+        RestoreJumpCount();
+    }
+
+    private void OnGroundTouched(RaycastHit2D hit)
+    {
+        RestoreJumpCount();
     }
 }
